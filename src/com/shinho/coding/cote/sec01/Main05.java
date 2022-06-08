f11a9414649da1ade9695099ddf70f16defaf799
92bogus <shk8170@gmail.com>
2022-06-08 15:48:31 +0900

    인프런 코딩테스트 추가

---
+++ src/com/shinho/coding/cote/sec01/Main05.java
@@ -0,0 +1,47 @@
+package com.shinho.coding.cote.sec01;
+
+import java.util.Scanner;
+
+public class Main05 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+
+        System.out.println(solution(str));
+    }
+
+    public static String solution(String str) {
+        String answer = "";
+
+        int lt = 0, rt = str.length() - 1;
+        char[] s = str.toCharArray();
+        while (lt < rt) {
+            if (isNotAlphabet(s[lt])) {
+                lt++;
+            } else if (isNotAlphabet(s[rt])) {
+                rt--;
+            } else {
+                char tmp = s[lt];
+                s[lt] = s[rt];
+                s[rt] = tmp;
+
+                lt++;
+                rt--;
+            }
+        }
+
+        answer = String.valueOf(s);
+        return answer;
+    }
+
+    public static boolean isNotAlphabet(char target) {
+        boolean answer = false;
+
+        target = Character.toUpperCase(target);
+        if (target < 65 || target > 90) {
+            answer = true;
+        }
+
+        return answer;
+    }
+}
